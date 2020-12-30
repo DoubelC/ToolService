cf6b66ec2ae1c68e8bff3c2929b5a1fda34939ce
chengchao <chengchao@cashcashpro.id>
2020-12-30 18:08:47 +0800

    增加获取当月天数方法

--- Tool/helper/functions.php
+++ Tool/helper/functions.php
@@ -1373,3 +1373,48 @@ function getOssThumbnail($url, $height, $width)
 
     return $url . '?x-oss-process=image/resize,m_lfit,h_' . $height . ',w_' . $width;
 }
+
+/**
+ * 获取当月天数
+ * @param $date
+ * @param string $rtype 1天数 2具体日期数组
+ * @return array|string
+ */
+
+function get_days($date, $rtype = '1')
+{
+    $r = array();
+    $tem = explode('-', $date);    //切割日期 得到年份和月份
+    $year = $tem['0'];
+    $month = $tem['1'];
+    if (in_array($month, array('1', '3', '5', '7', '8', '01', '03', '05', '07', '08', '10', '12'))) {
+        // $text = $year.'年的'.$month.'月有31天';
+        $text = '31';
+    } elseif ($month == 2) {
+        if ($year % 400 == 0 || ($year % 4 == 0 && $year % 100 !== 0))    //判断是否是闰年
+        {
+            // $text = $year.'年的'.$month.'月有29天';
+            $text = '29';
+        } else {
+            // $text = $year.'年的'.$month.'月有28天';
+            $text = '28';
+        }
+    } else {
+        // $text = $year.'年的'.$month.'月有30天';
+        $text = '30';
+    }
+    if ($rtype == '2') {
+        for ($i = 1; $i <= $text; $i++) {
+            if ($i < 10) {
+                $r[] = $year . "-" . $month . "-0" . $i;
+            } else {
+                $r[] = $year . "-" . $month . "-" . $i;
+            }
+
+        }
+    } else {
+        $r = $text;
+    }
+    return $r;
+}
+
